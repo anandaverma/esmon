211dd405d694de3f1ecba05e4828b696d8192e24
anandaverma <rahul.vit09@gmail.com>
2014-04-10 15:53:30 +0530

    moving report part from send mail function to create report function

--- src/com/fiberlink/elasticsearch/batchmonitor/report/ClusterHealthReport.java
+++ src/com/fiberlink/elasticsearch/batchmonitor/report/ClusterHealthReport.java
@@ -29,6 +29,7 @@ public class ClusterHealthReport implements Report {
 	private static Logger logger = Logger.getLogger(ClusterHealthReport.class);
 	private MyHttpClient httpclient = new MyHttpClient();
 	private Properties prop = new Properties();
+	private String msg = null;
 	public ClusterHealthReport() {
 		prop = MyUtils.loadProperties();
 	}
@@ -50,6 +51,78 @@ public class ClusterHealthReport implements Report {
 						prop.getProperty("ES_HOST_PORT"),
 						ElasticSearchSettings.clusterStatsEndPoint),
 				ClusterStats.class);
+		
+		// build health report
+				try {
+					msg = "Hi,<br />" + "Here is the health report of ES cluster on "
+							+ new SimpleDateFormat("dd MMM, yyyy HH:MM:ss")
+									.format((new Timestamp(Long
+											.parseLong(myClusterStats.getTimestamp()))))
+							+ "<br /><br />"
+							+ "<hr />Cluster Summary<br />"
+							+ "<b>Cluster Name:</b> "
+							+ myClusterStats.getCluster_name()
+							+ "<br />"
+							+ "<b>Cluster Status:</b> "
+							+ myClusterStats.getStatus()
+							+ "<br />"
+							+ "<b>Timeout:</b> "
+							+ myClusterHealth.getTimed_out()
+							+ "<br />"
+							+ "<b>Total Nodes Up:</b> "
+							+ myClusterHealth.getNumber_of_nodes()
+							+ "<br />"
+							+ "<b>Total Data Nodes Up:</b> "
+							+ myClusterHealth.getNumber_of_data_nodes()
+							+ "<br />"
+							+ "<b>Active  Nodes Up:</b> "
+							+ myClusterHealth.getNumber_of_nodes()
+							+ "<br />"
+							+ "<b>Total Document Indexed:</b>"
+							+ myClusterStats.getIndices().getDocs().getCount()
+							+ "<br />"
+							+ "<b>Index Size:</b> "
+							+ myClusterStats.getIndices().getStore().getSize()
+							+ "<br />"
+							+ "<b>Throttle Time:</b> "
+							+ myClusterStats.getIndices().getStore().getThrottle_time()
+							+ "<br />"
+							+ "<b>Active Primary Shards:</b> "
+							+ myClusterHealth.getActive_primary_shards()
+							+ "<br />"
+							+ "<b>Active Shards:</b> "
+							+ myClusterHealth.getActive_shards()
+							+ "<br />"
+							+ "<b>Relocating Shards:</b> "
+							+ myClusterHealth.getRelocating_shards()
+							+ "<br />"
+							+ "<b>Initializing Shards:</b> "
+							+ myClusterHealth.getInitializing_shards()
+							+ "<br />"
+							+ "<b>Unassigned Shards:</b> "
+							+ myClusterHealth.getUnassigned_shards()
+							+ "<br />"
+							+ "<hr />"
+							+ "Diagnostic Details<br />"
+							+ "os<br />"
+							+ "<b>Available Processor:</b> "
+							+ myClusterStats.getNodes().getOs()
+									.getAvailable_processors()
+							+ "<br />"
+							+ "jvm<br />"
+							+ "<b>Max Up-Time</b> "
+							+ myClusterStats.getNodes().getJvm().getMax_uptime()
+							+ "<br />"
+							+ "<b>Heap Used</b> "
+							+ myClusterStats.getNodes().getJvm().getMem()
+									.getHeap_used() + "<br />" + "<b>Heap Max</b> "
+							+ myClusterStats.getNodes().getJvm().getMem().getHeap_max()
+							+ "<br />" + "<b>Total Threads</b> "
+							+ myClusterStats.getNodes().getJvm().getThreads()
+							+ "<br />" + "";
+				} catch (Exception e) {
+					logger.error("unable to construct message", e);
+				}
 	}
 
 	@Override
@@ -79,78 +152,6 @@ public class ClusterHealthReport implements Report {
 
 	@Override
 	public void mailReport() {
-		// build health report
-		String msg = null;
-		try {
-			msg = "Hi,<br />" + "Here is the health report of ES cluster on "
-					+ new SimpleDateFormat("dd MMM, yyyy HH:MM:ss")
-							.format((new Timestamp(Long
-									.parseLong(myClusterStats.getTimestamp()))))
-					+ "<br /><br />"
-					+ "<hr />Cluster Summary<br />"
-					+ "<b>Cluster Name:</b> "
-					+ myClusterStats.getCluster_name()
-					+ "<br />"
-					+ "<b>Cluster Status:</b> "
-					+ myClusterStats.getStatus()
-					+ "<br />"
-					+ "<b>Timeout:</b> "
-					+ myClusterHealth.getTimed_out()
-					+ "<br />"
-					+ "<b>Total Nodes Up:</b> "
-					+ myClusterHealth.getNumber_of_nodes()
-					+ "<br />"
-					+ "<b>Total Data Nodes Up:</b> "
-					+ myClusterHealth.getNumber_of_data_nodes()
-					+ "<br />"
-					+ "<b>Active  Nodes Up:</b> "
-					+ myClusterHealth.getNumber_of_nodes()
-					+ "<br />"
-					+ "<b>Total Document Indexed:</b>"
-					+ myClusterStats.getIndices().getDocs().getCount()
-					+ "<br />"
-					+ "<b>Index Size:</b> "
-					+ myClusterStats.getIndices().getStore().getSize()
-					+ "<br />"
-					+ "<b>Throttle Time:</b> "
-					+ myClusterStats.getIndices().getStore().getThrottle_time()
-					+ "<br />"
-					+ "<b>Active Primary Shards:</b> "
-					+ myClusterHealth.getActive_primary_shards()
-					+ "<br />"
-					+ "<b>Active Shards:</b> "
-					+ myClusterHealth.getActive_shards()
-					+ "<br />"
-					+ "<b>Relocating Shards:</b> "
-					+ myClusterHealth.getRelocating_shards()
-					+ "<br />"
-					+ "<b>Initializing Shards:</b> "
-					+ myClusterHealth.getInitializing_shards()
-					+ "<br />"
-					+ "<b>Unassigned Shards:</b> "
-					+ myClusterHealth.getUnassigned_shards()
-					+ "<br />"
-					+ "<hr />"
-					+ "Diagnostic Details<br />"
-					+ "os<br />"
-					+ "<b>Available Processor:</b> "
-					+ myClusterStats.getNodes().getOs()
-							.getAvailable_processors()
-					+ "<br />"
-					+ "jvm<br />"
-					+ "<b>Max Up-Time</b> "
-					+ myClusterStats.getNodes().getJvm().getMax_uptime()
-					+ "<br />"
-					+ "<b>Heap Used</b> "
-					+ myClusterStats.getNodes().getJvm().getMem()
-							.getHeap_used() + "<br />" + "<b>Heap Max</b> "
-					+ myClusterStats.getNodes().getJvm().getMem().getHeap_max()
-					+ "<br />" + "<b>Total Threads</b> "
-					+ myClusterStats.getNodes().getJvm().getThreads()
-					+ "<br />" + "";
-		} catch (Exception e) {
-			logger.error("unable to construct message", e);
-		}
 		if (msg != null && subscriptionProp.getProperty("subscribers") != null) {
 			MailClient.sendHTMLEmail(subscriptionProp.getProperty("subscribers"), REPORT_NAME,
 					MailClient.formatMail(msg));
